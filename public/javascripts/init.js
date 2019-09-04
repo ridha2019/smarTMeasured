838f948116f1327083608d4fcc107c63d3b081ba
Ridha Mattoussi <54769096+ridha2019@users.noreply.github.com>
2019-09-05 01:17:01 +0200

    Add files via upload

---
+++ public/javascripts/init.js
@@ -0,0 +1,12 @@
+//if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /firebase/init.js');
+ // Initialize Firebase		AIzaSyC4u4031dCaX_BupYVBvQIBWdHX_EBbxw8
+var config = {
+	apiKey: "AIzaSyBOvcyYDJ5rgQAY_5Pwdr6IY0PkArwlQqo",
+	authDomain: "antennameasured.firebaseapp.com",
+	databaseURL: "https://antennameasured.firebaseio.com",
+	projectId: "antennameasured",
+	storageBucket: "",
+	messagingSenderId: "448978381040",
+	appId: "1:448978381040:web:d20abf100a4aad4a"
+};
+firebase.initializeApp(config);
